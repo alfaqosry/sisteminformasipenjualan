73e6706486a85302b40e38d11768e1d248932e82
alfaqosry <81825300+alfaqosry@users.noreply.github.com>
2024-07-25 21:44:16 +0700

    update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -51,7 +51,7 @@
             <div class="row">
               <div class="col-8">
                 <div class="numbers">
-                  <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Penjualan</p>
+                  <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Penjualann</p>
                   <h5 class="font-weight-bolder mb-0">
                  {{$totalpenjualan}}
                    
